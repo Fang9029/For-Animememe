4624ab7fc76ba8157a2546694d8164fe3b68ee94
Fang9029 <47442159+Fang9029@users.noreply.github.com>
2019-02-08 01:03:37 -0500

    For you animememe bois and girls.
    ...What, need i say more

---
+++ 50TB homework.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<ctime>
+#include<string>
+#include<cstdlib>
+#include<limits>
+using namespace std;
+
+int main() {
+	
+	srand(time(NULL));
+	int funNum;
+	char Destiny;
+	 do{
+	 	cout << "Do you want to have fun?(y/n)" << endl;
+	 	cin >> Destiny;
+	 	if (Destiny == 'Y'|| Destiny =='y'){
+	 		funNum = (rand() % 262460) + 1; 
+	 		cout <<"www.nhentai/g/" << funNum << endl;
+		 }
+	 	else if(Destiny == 'N'|| Destiny == 'n'){
+	 		cout << "Sayonara,Weeb." << endl;
+	 		return 0;
+		 }
+		else {
+			cout << "Wrong command, you Baka. Try again." << endl;
+		}
+	
+		cin.clear();
+		cin.ignore(numeric_limits<char>::max(),'\n');
+	 }while(!(Destiny == 'N'|| Destiny == 'n'));
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
